a54e22ad9fdef4569cd9c97773fedb07b245e037
Pascal Leroy <phl+personal@google.com>
2005-09-04 16:46:06 +0200

    {9/4/2005 4:46:06 PM~9/4/2005 4:46:08 PM}(Committed by Phl)

---
+++ HP67 Class Library/Stack.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HP67_Class_Library
+{
+	/// <summary>
+	/// Summary description for Stack.
+	/// </summary>
+	public class Stack
+	{
+		public Stack()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
