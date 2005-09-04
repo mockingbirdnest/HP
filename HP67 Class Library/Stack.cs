49d5efa06d2d3f87117a868b02e1d7bdf16db7b0
Robin Leroy <egg.robin.leroy@gmail.com>
2005-09-04 20:54:34 +0200

    {9/4/2005 8:54:34 PM}(Committed by Robin)

--- HP67 Class Library/Stack.cs
+++ HP67 Class Library/Stack.cs
@@ -7,11 +7,100 @@ namespace HP67_Class_Library
 	/// </summary>
 	public class Stack
 	{
+		private enum Position
+		{
+			x = 0,
+			y = 1,
+			z = 2,
+			t = 3
+		}
+		private double lastX;
+		private double[] stack;
+
 		public Stack()
 		{
-			//
-			// TODO: Add constructor logic here
-			//
+			lastX = 0.0;
+			stack = new double[Position.t - Position.x + 1];
+			for (int i = 0; i < stack.Length; i++)
+			{
+				stack[i] = 0.0;
+			}
 		}
+
+		void ExchangeXY()
+		{
+			double temp;
+			temp = stack[(int)Position.x];
+			stack[(int)Position.x] = stack[(int)Position.y];
+			stack[(int)Position.y] = temp;
+		}
+
+		public void RollDown()
+		{
+			double temp;
+			temp = stack[(int)Position.x];
+			for (int i = (int)Position.x; i < (int)Position.t; i++)
+			{
+				stack[i] = stack[i + 1];
+			}
+			stack[(int)Position.t] = temp;
+		}
+
+		public void RollUp()
+		{
+			double temp;
+			temp = stack[(int)Position.t];
+			for (int i = (int)Position.t; i > (int)Position.x; i--)
+			{
+				stack[i] = stack[i - 1];
+			}
+			stack[(int)Position.x] = temp;
+		}
+		
+		public double LastX
+		{
+			get
+			{
+				return lastX;
+			}
+		}
+
+		public double X
+		{
+			get 
+			{
+				return stack[(int)Position.x];
+			}
+			set 
+			{
+				stack[(int)Position.x] = value;
+			}
+		}
+
+		public void Get(out double X)
+		{
+			X = stack[(int)Position.x];
+			lastX = stack[(int)Position.x];
+		}
+
+		public void Get(out double X, out double Y)
+		{
+			X = stack[(int)Position.x];
+			Y = stack[(int)Position.y];
+			lastX = stack[(int)Position.x];
+			for (int i = (int)Position.x; i < (int)Position.t; i++)
+			{
+				stack[i] = stack[i + 1];
+			}
+		}
+
+		public void Enter()
+		{
+			for (int i = (int)Position.t; i > (int)Position.x; i--)
+			{
+				stack[i] = stack[i - 1];
+			}
+		}
+
 	}
 }
