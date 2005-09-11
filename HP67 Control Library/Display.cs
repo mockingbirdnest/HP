1ad276a7ec9e7f8b85506d8f62f40643c244fb7c
Pascal Leroy <phl+personal@google.com>
2005-09-11 19:36:38 +0200

    {9/11/2005 7:36:38 PM~9/11/2005 7:37:10 PM}(Committed by Phl)

---
+++ HP67 Control Library/Display.cs
@@ -0,0 +1,438 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Data;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace HP67_Control_Library
+{
+	/// <summary>
+	/// The LED display for the HP67 calculator.
+	/// </summary>
+	public class Display : System.Windows.Forms.UserControl
+	{
+
+		public enum Format
+		{
+			Engineering,
+			Fixed,
+			Scientific
+		}
+
+		#region Private Data
+
+		private const int mantissaSignFirst = 0;
+		private const int mantissaSignLength = 1;
+		private const int mantissaFirst = 1;
+		private const int mantissaLength = 11;
+		private const int exponentSignFirst = 12;
+		private const int exponentSignLength = 1;
+		private const int exponentFirst = 13;
+		private const int exponentLength = 2;
+		private const double overflowLimit = 9.999999999E99;
+		private const double underflowLimit = 1.0E-99;
+
+		private double currentMantissa;
+		private byte digits;
+		private string engMantissaTemplate1;
+		private string engMantissaTemplate10;
+		private string engMantissaTemplate100;
+		private string exponentTemplate = " 00;-00";
+		private string fixMantissaTemplate;
+		private string fixUnderflowOverflowMantissaTemplate = " 0.000000000;-0.000000000";
+		private Format format;
+		private string negativeOverflow = "-9.999999999 99";
+		private bool overflows;
+		private double overflowValue;
+		private string positiveOverflow = " 9.999999999 99";
+		private string sciMantissaTemplate;
+
+		internal System.Windows.Forms.TextBox textBox;
+
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		#endregion
+
+		#region Constructors & Destructors
+
+		public Display()
+		{
+			// This call is required by the Windows.Forms Form Designer.
+			InitializeComponent();
+
+			DigitsCount = 2;
+			DigitsFormat = Format.Fixed;
+			Value = 0.0;
+			this.Size = textBox.Size;
+		}
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose (bool disposing)
+		{
+			if (disposing)
+			{
+				if (components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose (disposing);
+		}
+
+		#endregion
+
+		#region Component Designer generated code
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox
+			// 
+			this.textBox.BackColor = System.Drawing.Color.Black;
+			this.textBox.Font = new System.Drawing.Font("Quartz", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.textBox.ForeColor = System.Drawing.Color.Red;
+			this.textBox.Location = new System.Drawing.Point(0, 0);
+			this.textBox.Name = "textBox";
+			this.textBox.ReadOnly = true;
+			this.textBox.Size = new System.Drawing.Size(300, 40);
+			this.textBox.TabIndex = 0;
+			this.textBox.Text = "0.00";
+			this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox_MouseDown);
+			this.textBox.MouseLeave += new System.EventHandler(this.textBox_MouseLeave);
+			// 
+			// Display
+			// 
+			this.Controls.Add(this.textBox);
+			this.Font = new System.Drawing.Font("Quartz", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.ForeColor = System.Drawing.Color.Red;
+			this.Name = "Display";
+			this.Size = new System.Drawing.Size(300, 40);
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+		#region Event Handlers
+
+		// This stuff is intended to prevent selection of the text.  It doesn't work 100% of the
+		// time, but it's good enough in practice.  At any rate, setting the Value property clean
+		// things up.
+
+		private void textBox_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			textBox.Enabled = false;
+		}
+
+		private void textBox_MouseLeave(object sender, System.EventArgs e)
+		{
+			textBox.Enabled = true;
+		}
+
+		#endregion
+
+		#region Private Properties
+
+		private sbyte Exponent 
+		{
+			get
+			{
+				string exponent = Text.Substring 
+					(exponentSignFirst, exponentSignLength + exponentLength).Trim ();
+				if (exponent.Length == 0) 
+				{
+					return 0;
+				}
+				else
+				{
+					return sbyte.Parse (exponent);
+				}
+			}
+			set
+			{
+				if (value == 0 && format == Format.Fixed) 
+				{
+					Text =
+						Text.Substring (mantissaSignFirst, mantissaSignLength + mantissaLength) +
+						new String (' ', exponentSignLength + exponentLength);
+				}
+				else
+				{
+					string exponent = value.ToString
+						(exponentTemplate, NumberFormatInfo.InvariantInfo);
+
+					Text =
+						Text.Substring (mantissaSignFirst, mantissaSignLength + mantissaLength) +
+						exponent;
+				}
+			}
+		}
+
+		private new string Text 
+		{
+			get
+			{
+				string s = textBox.Text;
+				return s.PadRight (exponentFirst + exponentLength);
+			}
+			set
+			{
+				textBox.Text = value.PadRight (exponentFirst + exponentLength);
+			}
+		}
+
+		#endregion
+
+		#region Private Operations
+
+		private void SetMantissa (double to, bool fixedUnderflowOverflow) 
+		{
+			string mantissa = "Useless initialization required by the compiler";
+				
+			switch (format) 
+			{
+				case Format.Engineering:
+				{
+					double absTo = Math.Abs (to);
+
+					if (absTo < 10.0) 
+					{
+						mantissa = to.ToString
+							(engMantissaTemplate1, NumberFormatInfo.InvariantInfo);
+					}
+					else if (absTo < 100.0)
+					{
+						mantissa = to.ToString
+							(engMantissaTemplate10, NumberFormatInfo.InvariantInfo);
+					}
+					else
+					{
+						Trace.Assert (absTo < 1000.0);
+						mantissa = to.ToString
+							(engMantissaTemplate100, NumberFormatInfo.InvariantInfo);
+					}
+					break;
+				}
+				case Format.Fixed:
+				{
+					if (fixedUnderflowOverflow) 
+					{
+						mantissa = to.ToString
+							(fixUnderflowOverflowMantissaTemplate, NumberFormatInfo.InvariantInfo);
+					}
+					else
+					{
+						mantissa = to.ToString
+							(fixMantissaTemplate, NumberFormatInfo.InvariantInfo);
+						mantissa = mantissa.Substring
+							(mantissaSignFirst, mantissaSignLength + mantissaLength);
+					};
+					break;
+				}
+				case Format.Scientific:
+				{
+					mantissa = to.ToString
+						(sciMantissaTemplate, NumberFormatInfo.InvariantInfo);
+					break;
+				}
+			}
+
+			Text = 
+				mantissa +
+				Text.Substring (exponentSignFirst, exponentSignLength + exponentLength);
+			currentMantissa = to;
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		public byte DigitsCount
+		{
+			set
+			{
+				string zeros;
+				string blanks;
+
+				Trace.Assert (value <= 9);
+				digits = value;
+				blanks = new String (' ', 9 - value);
+
+				if (value == 0) 
+				{
+					// Make sure that we don't lose the period when the count is 0.
+					fixMantissaTemplate = " 0\\." + blanks + ";-0\\." + blanks;
+				}
+				else 
+				{
+					zeros = new String ('0', value);
+					fixMantissaTemplate = " 0." + zeros + blanks + ";-0." + zeros + blanks;
+				}
+
+				// Engineering is weird because the DigitsCount is the total number of digits
+				// displayed minus 1, regardless of the position of the decimal point.  Produce
+				// three templates corresponding to the three orders of magnitude of the mantissa
+				// in this format.
+				if (value <= 1)
+				{
+					engMantissaTemplate10 = " 00\\." + blanks + ";-00\\." + blanks;
+				}
+				else 
+				{
+					zeros = new String ('0', value - 1);
+					engMantissaTemplate10 = " 00." + zeros + blanks + ";-00." + zeros + blanks;					
+				}
+
+				if (value <= 2)
+				{
+					engMantissaTemplate100 = " 000\\." + blanks + ";-000\\." + blanks;
+				}
+				else 
+				{
+					zeros = new String ('0', value - 2);
+					engMantissaTemplate100 = " 000." + zeros + blanks + ";-000." + zeros + blanks;					
+				}
+
+				engMantissaTemplate1 = fixMantissaTemplate;
+				sciMantissaTemplate = fixMantissaTemplate;
+				Value = Value;
+			}
+		}
+
+		public Format DigitsFormat
+		{
+			set
+			{
+				format = value;
+				Value = Value;
+			}
+		}
+
+		public double Value 
+		{
+			get
+			{
+				if (overflows) 
+				{
+					return overflowValue;
+				}
+				else 
+				{
+					return currentMantissa * Math.Pow (10, Exponent);
+				}
+			}
+			set
+			{
+				double absValue = Math.Abs (value);
+				sbyte exponent;
+				bool fixedUnderflowOverflow;
+				double log10;
+				double mantissa;
+
+				// Not needed, unless the event handling mucked things up...
+				textBox.Enabled = true;
+
+				// Deal with possible underflow or overflow.
+				overflows = false;
+				if (absValue > overflowLimit) 
+				{
+					if (value > 0.0) 
+					{
+						Text = positiveOverflow;
+						overflowValue = double.PositiveInfinity;
+					}
+					else 
+					{
+						Text = negativeOverflow;
+						overflowValue = double.NegativeInfinity;
+					}
+					overflows = true;
+					return;
+				}
+
+				if (absValue < underflowLimit)
+				{
+					exponent = 0;
+					value = 0;
+				}
+				else
+				{
+
+					// Compute the exponent in base 10, so that the mantissa has one digit before
+					// the decimal point.
+					log10 = Math.Log10 (absValue);
+					if (log10 >= 0.0) 
+					{
+						exponent = (sbyte) Math.Floor (log10);
+					}
+					else
+					{
+						exponent = (sbyte) (-Math.Ceiling (-log10));
+					}
+				}
+
+				// Adjust the computed exponent based on the display format, and determine if the
+				// fixed format underflows or overflows.
+				fixedUnderflowOverflow = false;
+				switch (format) 
+				{
+					case Format.Engineering:
+					{
+						if (exponent % 3 != 0) 
+						{
+							exponent = (sbyte) (exponent - (exponent % 3) - (exponent >= 0 ? 0: 3));
+						}
+						break;
+					}
+					case Format.Fixed:
+					{
+						if (exponent < -digits - 1 || exponent > 9) 
+						{
+							fixedUnderflowOverflow = true;
+						}
+						else if (exponent == -digits - 1) 
+						{
+							// A subtlety here.  If digits is, say, 2, the values ±0.005 round to
+							// ±0.01, but the values ±0.004 underflow.
+							if (absValue < 0.5 * Math.Pow (10, -digits)) 
+							{
+								fixedUnderflowOverflow = true;
+							}
+						}
+						if (! fixedUnderflowOverflow)
+						{
+							exponent = 0;
+						}
+						break;
+					}
+					case Format.Scientific:
+					{
+						break;
+					}
+				}
+
+				// Display the appropriate exponent and mantissa using the current digit format and 
+				// count.
+				Exponent = exponent;
+				mantissa = value / Math.Pow (10, exponent);
+				SetMantissa (mantissa, fixedUnderflowOverflow);
+			}
+		}
+
+		#endregion
+
+	}
+}
---
+++ HP67 Testing/DisplayTest.cs
@@ -0,0 +1,221 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace HP67_Testing
+{
+	/// <summary>
+	/// Summary description for DisplayTest.
+	/// </summary>
+	public class DisplayTest : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.PropertyGrid PropertyGrid;
+		private HP67_Control_Library.Display DisplayUnderTest;
+		private System.Windows.Forms.NumericUpDown numericUpDown;
+		private System.Windows.Forms.Label labelDigits;
+		private System.Windows.Forms.RadioButton radioButtonScientific;
+		private System.Windows.Forms.RadioButton radioButtonFixed;
+		private System.Windows.Forms.RadioButton radioButtonEngineering;
+		private System.Windows.Forms.GroupBox groupBox;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public DisplayTest()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+
+			//
+			// TODO: Add any constructor code after InitializeComponent call
+			//
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.PropertyGrid = new System.Windows.Forms.PropertyGrid();
+			this.DisplayUnderTest = new HP67_Control_Library.Display();
+			this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+			this.labelDigits = new System.Windows.Forms.Label();
+			this.radioButtonScientific = new System.Windows.Forms.RadioButton();
+			this.radioButtonFixed = new System.Windows.Forms.RadioButton();
+			this.radioButtonEngineering = new System.Windows.Forms.RadioButton();
+			this.groupBox = new System.Windows.Forms.GroupBox();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+			this.groupBox.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// PropertyGrid
+			// 
+			this.PropertyGrid.CommandsVisibleIfAvailable = true;
+			this.PropertyGrid.Cursor = System.Windows.Forms.Cursors.HSplit;
+			this.PropertyGrid.LargeButtons = false;
+			this.PropertyGrid.LineColor = System.Drawing.SystemColors.ScrollBar;
+			this.PropertyGrid.Location = new System.Drawing.Point(8, 8);
+			this.PropertyGrid.Name = "PropertyGrid";
+			this.PropertyGrid.SelectedObject = this.DisplayUnderTest;
+			this.PropertyGrid.Size = new System.Drawing.Size(480, 360);
+			this.PropertyGrid.TabIndex = 4;
+			this.PropertyGrid.Text = "PropertyGrid";
+			this.PropertyGrid.ViewBackColor = System.Drawing.SystemColors.Window;
+			this.PropertyGrid.ViewForeColor = System.Drawing.SystemColors.WindowText;
+			// 
+			// DisplayUnderTest
+			// 
+			this.DisplayUnderTest.Font = new System.Drawing.Font("Quartz", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.DisplayUnderTest.ForeColor = System.Drawing.Color.Red;
+			this.DisplayUnderTest.Location = new System.Drawing.Point(72, 392);
+			this.DisplayUnderTest.Name = "DisplayUnderTest";
+			this.DisplayUnderTest.Size = new System.Drawing.Size(310, 50);
+			this.DisplayUnderTest.TabIndex = 5;
+			this.DisplayUnderTest.Value = 0;
+			// 
+			// numericUpDown
+			// 
+			this.numericUpDown.Location = new System.Drawing.Point(16, 400);
+			this.numericUpDown.Maximum = new System.Decimal(new int[] {
+																		  9,
+																		  0,
+																		  0,
+																		  0});
+			this.numericUpDown.Name = "numericUpDown";
+			this.numericUpDown.Size = new System.Drawing.Size(40, 20);
+			this.numericUpDown.TabIndex = 6;
+			this.numericUpDown.Value = new System.Decimal(new int[] {
+																		2,
+																		0,
+																		0,
+																		0});
+			this.numericUpDown.ValueChanged += new System.EventHandler(this.numericUpDown_ValueChanged);
+			// 
+			// labelDigits
+			// 
+			this.labelDigits.Location = new System.Drawing.Point(16, 384);
+			this.labelDigits.Name = "labelDigits";
+			this.labelDigits.Size = new System.Drawing.Size(40, 16);
+			this.labelDigits.TabIndex = 7;
+			this.labelDigits.Text = "Digits";
+			this.labelDigits.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// radioButtonScientific
+			// 
+			this.radioButtonScientific.Location = new System.Drawing.Point(8, 56);
+			this.radioButtonScientific.Name = "radioButtonScientific";
+			this.radioButtonScientific.Size = new System.Drawing.Size(90, 24);
+			this.radioButtonScientific.TabIndex = 2;
+			this.radioButtonScientific.Text = "Scientific";
+			this.radioButtonScientific.CheckedChanged += new System.EventHandler(this.radioButtonScientific_CheckedChanged);
+			// 
+			// radioButtonFixed
+			// 
+			this.radioButtonFixed.Checked = true;
+			this.radioButtonFixed.Location = new System.Drawing.Point(8, 32);
+			this.radioButtonFixed.Name = "radioButtonFixed";
+			this.radioButtonFixed.Size = new System.Drawing.Size(90, 24);
+			this.radioButtonFixed.TabIndex = 1;
+			this.radioButtonFixed.TabStop = true;
+			this.radioButtonFixed.Text = "Fixed";
+			this.radioButtonFixed.CheckedChanged += new System.EventHandler(this.radioButtonFixed_CheckedChanged);
+			// 
+			// radioButtonEngineering
+			// 
+			this.radioButtonEngineering.Location = new System.Drawing.Point(8, 8);
+			this.radioButtonEngineering.Name = "radioButtonEngineering";
+			this.radioButtonEngineering.Size = new System.Drawing.Size(90, 24);
+			this.radioButtonEngineering.TabIndex = 0;
+			this.radioButtonEngineering.Text = "Engineering";
+			this.radioButtonEngineering.CheckedChanged += new System.EventHandler(this.radioButtonEngineering_CheckedChanged);
+			// 
+			// groupBox
+			// 
+			this.groupBox.Controls.Add(this.radioButtonScientific);
+			this.groupBox.Controls.Add(this.radioButtonFixed);
+			this.groupBox.Controls.Add(this.radioButtonEngineering);
+			this.groupBox.Location = new System.Drawing.Point(376, 368);
+			this.groupBox.Name = "groupBox";
+			this.groupBox.Size = new System.Drawing.Size(104, 88);
+			this.groupBox.TabIndex = 8;
+			this.groupBox.TabStop = false;
+			// 
+			// DisplayTest
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(492, 466);
+			this.Controls.Add(this.groupBox);
+			this.Controls.Add(this.labelDigits);
+			this.Controls.Add(this.numericUpDown);
+			this.Controls.Add(this.DisplayUnderTest);
+			this.Controls.Add(this.PropertyGrid);
+			this.Name = "DisplayTest";
+			this.Text = "DisplayTest";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+			this.groupBox.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() 
+		{
+			Application.Run(new DisplayTest());
+		}
+
+		private void numericUpDown_ValueChanged(object sender, System.EventArgs e)
+		{
+			DisplayUnderTest.DigitsCount = (byte) numericUpDown.Value;
+		}
+
+		private void radioButtonEngineering_CheckedChanged(object sender, System.EventArgs e)
+		{
+			if (radioButtonEngineering.Checked) 
+			{
+				DisplayUnderTest.DigitsFormat = HP67_Control_Library.Display.Format.Engineering;
+			}
+		}
+
+		private void radioButtonFixed_CheckedChanged(object sender, System.EventArgs e)
+		{
+			if (radioButtonFixed.Checked) 
+			{
+				DisplayUnderTest.DigitsFormat = HP67_Control_Library.Display.Format.Fixed;
+			}
+		}
+
+		private void radioButtonScientific_CheckedChanged(object sender, System.EventArgs e)
+		{
+			if (radioButtonScientific.Checked) 
+			{
+				DisplayUnderTest.DigitsFormat = HP67_Control_Library.Display.Format.Scientific;
+			}
+		}
+	}
+}
