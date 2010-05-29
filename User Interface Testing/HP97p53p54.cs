b508c33396fe4c99bb6ce7b0bc55aafd46933806
Pascal Leroy <phl+personal@google.com>
2010-05-29 14:27:43 +0200

    Two more tests.

---
+++ User Interface Testing/HP97p53p54.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+
+namespace User_Interface_Testing
+{
+    public partial class HP97
+    {
+        [TestMethod]
+        public void p53p54 ()
+        {
+            // Two-Number Functions and the Stack
+            UIMap.CLx ();
+            UIMap.AssertNumeric (" 0.00          ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 0.00          ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 0.00          ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 0.00          ");
+            UIMap.Three ();
+            UIMap.Four ();
+            UIMap.AssertNumeric (" 34.           ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 34.00         ");
+            UIMap.AssertPrinter ("                 CLX",
+                                 "                ENT↑",
+                                 "                ENT↑",
+                                 "                ENT↑",
+                                 "          34.00 ENT↑");
+
+            UIMap.Two ();
+            UIMap.One ();
+            UIMap.AssertNumeric (" 21.           ");
+            UIMap.PrintStack ();
+            UIMap.AssertNumeric (" 21.00         ");
+            UIMap.AssertPrinter ("          21.00 PRST",
+                                 "",
+                                 "           0.00   T ",
+                                 "           0.00   Z ",
+                                 "          34.00   Y ",
+                                 "          21.00   X ",
+                                 "");
+
+            UIMap.Addition ();
+            UIMap.AssertNumeric (" 55.00         ");
+            UIMap.PrintStack ();
+            UIMap.AssertNumeric (" 55.00         ");
+            UIMap.AssertPrinter ("                  + ",
+                                 "                PRST",
+                                 "",
+                                 "           0.00   T ",
+                                 "           0.00   Z ",
+                                 "           0.00   Y ",
+                                 "          55.00   X ",
+                                 "");
+        }
+    }
+}
---
+++ User Interface Testing/HP97p55p57.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+
+namespace User_Interface_Testing
+{
+    public partial class HP97
+    {
+        [TestMethod]
+        public void p55p57 ()
+        {
+            // Chain Arithmetic
+            UIMap.One ();
+            UIMap.Six ();
+            UIMap.AssertNumeric (" 16.           ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 16.00         ");
+            UIMap.Three ();
+            UIMap.Zero ();
+            UIMap.AssertNumeric (" 30.           ");
+            UIMap.Addition ();
+            UIMap.AssertNumeric (" 46.00         ");
+            UIMap.OneOne ();
+            UIMap.AssertNumeric (" 11.           ");
+            UIMap.Addition ();
+            UIMap.AssertNumeric (" 57.00         ");
+            UIMap.One ();
+            UIMap.Seven ();
+            UIMap.AssertNumeric (" 17.           ");
+            UIMap.Addition ();
+            UIMap.PRINTx ();
+            UIMap.AssertNumeric (" 74.00         ");
+            UIMap.AssertPrinter ("          16.00 ENT↑",
+                                 "          30.00   + ",
+                                 "          11.00   + ",
+                                 "          17.00   + ",
+                                 "          74.00  ***");
+
+            UIMap.CLx ();
+            UIMap.AssertPrinter ("                 CLX");  // Not in the manual.
+
+            UIMap.One ();
+            UIMap.Six ();
+            UIMap.AssertNumeric (" 16.           ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 16.00         ");
+            UIMap.Three ();
+            UIMap.Zero ();
+            UIMap.AssertNumeric (" 30.           ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 30.00         ");
+            UIMap.One ();
+            UIMap.One ();
+            UIMap.AssertNumeric (" 11.           ");
+            UIMap.ENTER ();
+            UIMap.AssertNumeric (" 11.00         ");
+            UIMap.One ();
+            UIMap.Seven ();
+            UIMap.AssertNumeric (" 17.           ");
+            UIMap.Addition ();
+            UIMap.AssertNumeric (" 28.00         ");
+            UIMap.Addition ();
+            UIMap.AssertNumeric (" 58.00         ");
+            UIMap.Addition ();
+            UIMap.PRINTx ();
+            UIMap.AssertNumeric (" 74.00         ");
+            UIMap.AssertPrinter ("          16.00 ENT↑",
+                                 "          30.00 ENT↑",
+                                 "          11.00 ENT↑",
+                                 "          17.00   + ",
+                                 "                  + ",
+                                 "                  + ",
+                                 "          74.00  ***");
+        }
+    }
+}
