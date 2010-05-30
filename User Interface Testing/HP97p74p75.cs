dad8a32deab56199d8b9ae25792e2656edc1a8c2
Pascal Leroy <phl+personal@google.com>
2010-05-30 19:05:54 +0200

    A new test, and a bit of cleanup.

---
+++ User Interface Testing/HP97p74p75.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+
+namespace User_Interface_Testing
+{
+    public partial class HP97
+    {
+        [TestMethod]
+        public void p74p75 ()
+        {
+            // Storage Register Overflow
+            UIMap.Seven ();
+            UIMap.Period ();
+            UIMap.Three ();
+            UIMap.Three ();
+            UIMap.AssertNumeric (" 7.33          ");
+            UIMap.EEX ();
+            UIMap.Five ();
+            UIMap.Two ();
+            UIMap.AssertNumeric (" 7.33        52");
+            UIMap.STO ();
+            UIMap.One ();
+            UIMap.AssertNumeric (" 7.330000000 52");
+            UIMap.EEX ();
+            UIMap.Five ();
+            UIMap.Zero ();
+            UIMap.AssertNumeric (" 1.          50");
+            UIMap.STO ();
+            UIMap.Multiplication ();
+            UIMap.One ();
+            UIMap.AssertText (" Error         ");
+            UIMap.AssertPrinter ("        7.33+52 STO1",
+                                 "          1.+50 ST×1",
+                                 "               ERROR");
+
+            UIMap.CLx ();
+            UIMap.AssertNumeric (" 1.000000000 50");
+            UIMap.RCL ();
+            UIMap.One ();
+            UIMap.AssertNumeric (" 7.330000000 52");
+            UIMap.AssertPrinter ("                RCL1");
+        }
+    }
+}
