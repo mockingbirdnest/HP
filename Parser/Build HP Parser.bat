d99d9ab61e940ad44da18641c2dce9d5bf291261
Pascal Leroy <phl+personal@google.com>
2011-05-29 11:17:34 +0200

    Fix building tools and move to proper way of embedding the CGT resource.

--- Parser/Build HP Parser.bat
+++ Parser/Build HP Parser.bat
@@ -1,5 +1,7 @@
 REM Construction of the grammar-related files.
+set GOLDDIR=C:\Program Files (x86)\GOLD Parser Builder\Cmd
+set PATH=%GOLDDIR%;%PATH%
 call goldbuilder HP.grm HP.cgt
 call createskelprog HP.cgt "C-Sharp - Calitha Interface Based.pgt" Parser.cs
 call createskelprog HP.cgt "C-Sharp - Calitha Interface Implementation.pgt" Template.cs
-resxgen /i:HP.cgt /o:Parser.resx /n:CGT
+REM resxgen /i:HP.cgt /o:Parser.resx /n:CGT
